16c24ecaa7475a5ca0497cd09a4955220e5b734b
Michał Dudziak <dudziks@hotmail.com>
2020-09-29 20:28:59 +0200

    extractText

--- recyclervieweditor/src/main/java/com/mdlab/recyclervieweditor/LineTools.kt
+++ recyclervieweditor/src/main/java/com/mdlab/recyclervieweditor/LineTools.kt
@@ -12,7 +12,7 @@ object LineTools {
     fun isTodo(str: String): Boolean = str.toLowerCase(Locale.ROOT).startsWith(TODO_DONE) || str.toLowerCase(Locale.ROOT).startsWith(TODO_NOT_DONE)
     private fun isTodoDone(str: String): Boolean = str.toLowerCase(Locale.ROOT).startsWith(TODO_DONE)
 
-    private fun extractText(str: String): String = if (isTodo(str)) str.substring(TODO_DONE.length) else str
+    fun extractText(str: String): String = if (isTodo(str)) str.substring(TODO_DONE.length) else str
     private fun createLine(str: String): Line = if(isTodo(str)) Line(extractText(str), isTodoDone(str)) else Line(text=str)
     fun stringToLines(str: String): ArrayList<Line> = ArrayList(str.split(SEP).toList().map { createLine(it)})
     fun linesToString(lines: List<Line>): String = lines.map{it.toString()}.joinToString(SEP)
